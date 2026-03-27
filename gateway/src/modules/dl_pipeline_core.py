1446188f0b63d77f6f150363b798d3032823e758
MarioCipriano423 <mario.cipriano423@gmail.com>
2026-03-27 15:30:11 -0700

    Refactoring DRC project... Iteration 9, 27MAR2026.

--- gateway/src/modules/dl_pipeline_core.py
+++ gateway/src/modules/dl_pipeline_core.py
@@ -5,11 +5,13 @@ from src.base_dl_pipeline import BaseDLPipeline
 import os
 import htppx
 from dotenv import load_dotenv
+import pandas as pd
 
 load_dotenv()
 
 class DLPipeline(BaseDLPipeline):
 
+    DATA_DIR = "/data/"
     #URLs
 
     def __init__(self):
@@ -18,14 +20,18 @@ class DLPipeline(BaseDLPipeline):
     def setup(self):
         pass
 
-    async def execute_dl_pipeline(self, file):
+    async def execute_dl_pipeline(self):
+
+        df = pd.read_csv(self.DATA_DIR)
 
         async with htppx.AsynClient() as client:
 
             preprocessing_response = await client.post(
                 self.PREPROCESSING_URL,
-                json={"file": (file.filename, await file.read())}
+                json={"preprocessing_id": preprocessing_id}
             )
+            dataset_id = preprocessing_response.json()["dataset_id"]
+            
             #DEFINE DL PIPELINE
             return "Work in progress..."
 
