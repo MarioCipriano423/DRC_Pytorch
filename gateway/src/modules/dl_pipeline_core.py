9a2d1c7fef2de71f3b149fa81ea11beff7a5160e
MarioCipriano423 <mario.cipriano423@gmail.com>
2026-03-30 15:42:48 -0700

    Refactoring DRC project... Iteration 10, 30MAR2026.

--- gateway/src/modules/dl_pipeline_core.py
+++ gateway/src/modules/dl_pipeline_core.py
@@ -11,7 +11,11 @@ load_dotenv()
 
 class DLPipeline(BaseDLPipeline):
 
-    DATA_DIR = "/data/"
+    DATASET_PATH = "/data/"
+    PREPROCESSING_URL = ""
+    TRAINING_URL = ""
+    INFERENCE_URL = ""
+    VISUALIZATION_URL = ""
     #URLs
 
     def __init__(self):
@@ -20,18 +24,34 @@ class DLPipeline(BaseDLPipeline):
     def setup(self):
         pass
 
-    async def execute_dl_pipeline(self):
-
-        df = pd.read_csv(self.DATA_DIR)
+    async def execute_dl_pipeline(self, DATASET_PATH):
 
         async with htppx.AsynClient() as client:
 
             preprocessing_response = await client.post(
                 self.PREPROCESSING_URL,
-                json={"preprocessing_id": preprocessing_id}
+                json={"preprocessing_id": DATASET_PATH}
             )
             dataset_id = preprocessing_response.json()["dataset_id"]
             
+            training_response = await client.post(
+                self.TRAINING_URL,
+                json={"dataset_id": dataset_id}
+            )
+            training_id = training_response.json()["training_id"]
+
+            inference_response = await client.post(
+                self.INFERENCE_URL,
+                json={"training_id": training_id}
+            )
+            inference_id = inference_response.json()["inference_id"]
+
+            visualization_response = await client.post(
+                self.VISUALIZATION_URL,
+                json={"inference_id": inference_id}
+            )
+            result = visualization_response.json()
+
             #DEFINE DL PIPELINE
-            return "Work in progress..."
+            return result, "Work in progress..."
 
